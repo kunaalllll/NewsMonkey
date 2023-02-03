ce2cb530ce99f93806f763d67e6dc0188661a0b6
Kunal Mathur <kunalsrtk1404@gmail.com>
2023-02-04 04:33:02 +0530

    added categories and refactored the code

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem " }}>
+        <div className="card">
           <img
             src={
               imageUrl
@@ -16,9 +17,22 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">
+              {title} <span className="badge text-bg-warning">{source}</span>
+            </h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
               Read More
             </a>
           </div>
